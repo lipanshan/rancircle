0cfc64e59da7508dcd05fbbd02905dbad2c79ff2
lipanshan <15003167565@163.com>
2018-07-04 17:44:43 +0800

    solve jump error

--- rancircle/component/infoitem/infoitem.js
+++ rancircle/component/infoitem/infoitem.js
@@ -2,7 +2,27 @@ Component({
   properties: {
     info: {
       type: Object,
-      value: {}
+      value: {
+        avatar: '../../image/avatar.jpg',
+        userName: '樱木花道',
+        time: '06:56',
+        address: '广东-汕头',
+        title: '今天，我是浏览等级分类',
+        subtitle: '跑班今天开始打卡，小伙伴们加油啊，跑班今天开始打卡，小伙伴们加油啊，跑班今天开始打卡，小伙伴们加油啊跑班今天开始打卡，小伙伴们加油啊，跑班今天开始打卡，小伙伴们加油啊' ,
+        images: [
+          'show1.jpg',
+          'show2.jpg',
+          'show3.jpg',
+          'show4.jpg',
+          'show5.jpg',
+          'show6.jpg',
+          'show7.jpg',
+          'show8.jpg',
+          'show9.jpg'
+        ],
+        favorite: '1000',
+        talk: '179'
+      }
     }
   },
   data: {
